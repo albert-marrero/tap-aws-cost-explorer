b6ab610ace195f4199745dc55e7a473cf4912970
Gary James <gary.james19@gmail.com>
2022-08-21 04:16:12 +0100

    Feature - Convert stream to incremental (#4)

--- tap_aws_cost_explorer/streams.py
+++ tap_aws_cost_explorer/streams.py
@@ -1,8 +1,10 @@
 """Stream type classes for tap-aws-cost-explorer."""
 
+import datetime
 from pathlib import Path
 from typing import Optional, Iterable
 
+import pendulum
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_aws_cost_explorer.client import AWSCostExplorerStream
@@ -11,26 +13,33 @@ class CostAndUsageWithResourcesStream(AWSCostExplorerStream):
     """Define custom stream."""
     name = "cost"
     primary_keys = ["metric_name", "time_period_start"]
-    replication_key = None
+    replication_key = "time_period_start"
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     # schema_filepath = SCHEMAS_DIR / "users.json"
     schema = th.PropertiesList(
-        th.Property("time_period_start", th.StringType),
-        th.Property("time_period_end", th.StringType),
+        th.Property("time_period_start", th.DateTimeType),
+        th.Property("time_period_end", th.DateTimeType),
         th.Property("metric_name", th.StringType),
         th.Property("amount", th.StringType),
         th.Property("amount_unit", th.StringType),
     ).to_dict()
 
+    def _get_end_date(self):
+        if self.config.get("end_date") is None:
+            return datetime.datetime.today() - datetime.timedelta(days=1)
+        return th.cast(datetime.datetime, pendulum.parse(self.config["end_date"]))
+
     def get_records(self, context: Optional[dict]) -> Iterable[dict]:
         """Return a generator of row-type dictionary objects."""
         next_page = True
+        start_date = self.get_starting_timestamp(context)
+        end_date = self._get_end_date()
 
         while next_page:
             response = self.conn.get_cost_and_usage(
                 TimePeriod={
-                    'Start': self.config.get("start_date"),
-                    'End': self.config.get("end_date")
+                    'Start': start_date.strftime("%Y-%m-%d"),
+                    'End': end_date.strftime("%Y-%m-%d")
                 },
                 Granularity=self.config.get("granularity"),
                 Metrics=self.config.get("metrics"),
